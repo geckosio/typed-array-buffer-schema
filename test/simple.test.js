6d5bbb1fdf1447b9905e25326037d7199081e48b
Yannick <20306025+yandeu@users.noreply.github.com>
2020-08-30 13:41:03 +0200

    Update simple.test.js

--- test/simple.test.js
+++ test/simple.test.js
@@ -14,16 +14,23 @@ describe('simple test', () => {
     y: int16
   })
 
+  const listSchema = BufferSchema.schema('list', {
+    value: uint8
+  })
+
   const snapshotSchema = BufferSchema.schema('snapshot', {
     time: uint16,
-    data: { players: [playerSchema], castles: [castleSchema] }
+    single: uint8,
+    data: { list: [listSchema], players: [playerSchema], castles: [castleSchema] }
   })
 
   const SnapshotModel = new Model(snapshotSchema)
 
   const snap = {
     time: 1234,
+    single: 0,
     data: {
+      list: [{ value: 1 }, { value: 2 }],
       castles: [{ id: 2, health: 81 }],
       players: [
         {
@@ -50,12 +57,14 @@ describe('simple test', () => {
   test('should return a buffer', () => {
     buffer = SnapshotModel.toBuffer(snap)
     const uint8 = new Uint8Array(buffer)
+
     expect(typeof buffer).toBe('object')
-    expect(uint8.buffer.byteLength).toBe(29)
+    expect(uint8.buffer.byteLength).toBe(37)
   })
 
   test('should fromBuffer', () => {
     data = SnapshotModel.fromBuffer(buffer)
+
     expect(data.time).toBe(1234)
     expect(data.data.players[0].x).toBe(145)
   })
