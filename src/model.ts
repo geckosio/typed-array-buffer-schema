c82d0b257c3e8b4ee82de8f6315585ba2e74e378
Yannick <20306025+yandeu@users.noreply.github.com>
2020-11-03 17:58:27 +0100

    Make the schema public

--- src/model.ts
+++ src/model.ts
@@ -2,7 +2,7 @@ import { Schema } from './schema'
 import { Serialize } from './serialize'
 
 export class Model extends Serialize {
-  constructor(protected schema: Schema) {
+  constructor(public schema: Schema) {
     super(schema)
   }
 }
