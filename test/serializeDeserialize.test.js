f2bc451401f1f81fa12786d2d2eaff622cdd0a3a
yandeu <20306025+yandeu@users.noreply.github.com>
2021-11-29 22:15:46 +0100

    Update serializeDeserialize.test.js

--- test/serializeDeserialize.test.js
+++ test/serializeDeserialize.test.js
@@ -40,6 +40,10 @@ describe('serialize deserialize', () => {
 })
 
 describe('serialize deserialize (complex)', () => {
+  const TimerSchema = BufferSchema.schema('timer', {
+    time: uint32
+  })
+
   const castleSchema = BufferSchema.schema('castle', {
     name: string8,
     health: uint8
@@ -54,17 +58,24 @@ describe('serialize deserialize (complex)', () => {
   const gameSchema = BufferSchema.schema('snapshot', {
     name: string8,
     players: [playerSchema],
+    time: uint32,
+    stats: TimerSchema,
     castles: [castleSchema]
   })
 
   const gameModel = new Model(gameSchema)
 
+  const timeInSeconds = Math.floor(new Date().getTime() / 1000)
+
   const randomOrder = {
+    stats: { time: timeInSeconds },
+    time: timeInSeconds,
     castles: [
-      { name: 'beauty      ', health: 100 },
-      { health: 78, name: 'beauty2     ' }
+      { name: 'beauty', health: 100 },
+      { health: 78, name: 'beauty2' },
+      { health: 88, name: 'very_long_name' }
     ],
-    name: 'myGame      ',
+    name: 'myGame',
     players: [
       { id: 25, x: 788, y: -14 },
       { x: 1, y: 2, id: 87 }
@@ -77,6 +88,9 @@ describe('serialize deserialize (complex)', () => {
 
     expect(deserialized.players[0].id).toBe(25)
     expect(deserialized.castles[1].name.trim()).toBe('beauty2')
+    expect(deserialized.castles[2].name.trim()).toBe('very_long_name'.slice(0, 12))
     expect(deserialized.name.trim()).toBe('myGame')
+    expect(deserialized.time).toBe(timeInSeconds)
+    expect(deserialized.stats.time).toBe(timeInSeconds)
   })
 })
