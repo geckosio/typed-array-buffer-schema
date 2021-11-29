fdab986715eba2d65769aef59eef5829a65211d6
yandeu <20306025+yandeu@users.noreply.github.com>
2021-11-29 15:54:59 +0100

    Fix webpack bundling

--- webpack.bundle.tmp.js
+++ webpack.bundle.tmp.js
@@ -1,3 +1,3 @@
 const config = require('./webpack.bundle.js')
 
-module.exports = { ...config, output: { ...config.output, filename: 'snapshot-interpolation.tmp.js' } }
+module.exports = { ...config, output: { ...config.output, filename: 'typed-array-buffer-schema.tmp.js' } }
