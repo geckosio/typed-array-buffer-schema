9827e7a2a72304a17202b5d393b277db082f1434
yandeu <20306025+yandeu@users.noreply.github.com>
2022-12-10 13:21:26 +0100

    Export bool (closes #20)

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,6 +1,21 @@
 import { Lib as BufferSchema } from './lib'
 import { Model } from './model'
-import { int8, uint8, int16, uint16, int32, uint32, int64, uint64, float32, float64, string8, string16 } from './views'
+import {
+  int8,
+  uint8,
+  int16,
+  uint16,
+  int32,
+  uint32,
+  int64,
+  uint64,
+  float32,
+  float64,
+  string8,
+  string16,
+  bool8,
+  bool16
+} from './views'
 
 export default {
   BufferSchema,
@@ -16,5 +31,7 @@ export default {
   float32,
   float64,
   string8,
-  string16
+  string16,
+  bool8,
+  bool16
 }
